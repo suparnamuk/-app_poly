490597fdbf1ea8514acdf3e962c5aa91e4b80bbf
suparna@stepforwardsolution.com <suparna@stepforwardsolution.com>
2022-05-15 06:10:54 +0000

    intital commit

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,12 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  
+   scope '/api' do
+    resources :poly
+   end
 
-    namespace :bx_block_poly do
+
+      namespace :bx_block_poly do
         post 'poly_url', to:'poly#poly_url'
      end
 
